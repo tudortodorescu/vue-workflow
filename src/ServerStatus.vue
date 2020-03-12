1ffbf45cc481421f739985da67173a988a1ea3f1
todorescu <todorescu.consulting@gmail.com>
2020-03-12 15:25:46 +0200

    update

---
+++ src/ServerStatus.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Server Status {{ status }}</p>
+    <hr />
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: "critical"
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = "normal";
+    }
+  }
+};
+</script>
+
+<style lang="sass">
+</style>
